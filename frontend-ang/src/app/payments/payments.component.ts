21ccd2fdda21886e2b2b721f78555f232be439cf
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-04-21 21:16:14 +0000

    ajout du filtre de recherche

--- frontend-ang/src/app/payments/payments.component.ts
+++ frontend-ang/src/app/payments/payments.component.ts
@@ -32,5 +32,9 @@ export class PaymentsComponent implements OnInit{
         }
       })
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
